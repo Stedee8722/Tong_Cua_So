d4bb1b69f904098593406b1cbcb278f55c675ca4
Stedee <doanthanhbnh@gmail.com>
2024-11-01 23:35:38 +0700

    add missing condition

--- Source.c
+++ Source.c
@@ -21,6 +21,10 @@ int main() {
 		a = a / 10;
 		if (!found) {
 			if (char_of_a != 0) {
+				if (i == n) {
+					printf_s("Khong tim duoc");
+					return;
+				}
 				num_shifted = char_of_a - 1;
 				nums_of_zeros = i;
 				found = true;
@@ -41,7 +45,10 @@ int main() {
 			}
 		}
 	}
+	// adding the rest of the num of char back as 0s
 	a = a * pow(10, nums_of_nines + nums_of_zeros);
+
+	// calculating the new additions to make it smallest
 	first_char = debt % 9;
 	if (first_char + num_shifted > 9) {
 		addition = abs(num_shifted - first_char);
@@ -53,6 +60,8 @@ int main() {
 		for (int i = 1; i <= debt / 9; i++)
 			addition =+ addition * 10 + 9;
 	}
+
+	// you know what this is lmao
 	printf_s("%lld", a + addition);
 
 }
